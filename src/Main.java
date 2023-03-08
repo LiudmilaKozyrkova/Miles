2f86a99bc42a70ef27a01dbe998db09da551def7
Liudmila Kozyrkova <ludatmb@yandex.ru>
2023-03-08 18:06:34 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketCost = 16666;
+        int milesPerRubles = 20;
+        int count = ticketCost / milesPerRubles;
+
+        System.out.println("Количество миль за билет: " + count);
+    }
+}
